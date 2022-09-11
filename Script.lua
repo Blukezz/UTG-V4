3f776d513e7dc043c5fba93f33e433f16a54c089
Blukezz <86196314+Blukezz@users.noreply.github.com>
2022-09-11 05:33:01 -0700

    Update and rename Script to Script.lua

---
+++ Script.lua
@@ -0,0 +1,43 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Blukezz/UTG-V4/main/Lib"))()
+
+local Main_ = libary:CreateMain()
+
+Main_:CreateButton("Neptunion V", "Corrupt Demonic Greatsword", "", "", "", "", function()
+    loadstring(game:HttpGetAsync("https://pastebin.com/raw/AdLeNbD7"))()
+end)
+
+Main_:CreateButton("Rb Neptunion", "Russos Sword of Truth", "", "", "", "", function()
+    loadstring(game:HttpGetAsync("https://pastebin.com/raw/ztTyE58C"))()
+end)
+
+Main_:CreateButton("Krystal Dance", "None", "", "", "", "", function()
+    loadstring(game:HttpGetAsync("https://pastebin.com/raw/FV3HLmTH"))()
+end)
+
+Main_:CreateButton("Walk On Walls", "None", "", "", "", "", function()
+    loadstring(game:HttpGetAsync("https://pastebin.com/raw/RY9cBbdG"))()
+end)
+
+Main_:CreateButton("Sonic", "None", "", "", "", "", function()
+    loadstring(game:HttpGetAsync("https://pastebin.com/raw/SyF5t70A"))()
+end)
+
+Main_:CreateButton("Gale Fighter", "None", "", "", "", "", function()
+    loadstring(game:HttpGet("https://pastebin.com/raw/PjJNLaFa", true))()
+end)
+
+Main_:CreateButton("Among Us", "None", "", "", "", "", function()
+    loadstring(game:HttpGet("https://pastebin.com/raw/JXUMavf3", true))()
+end)
+
+Main_:CreateButton("Killbot V2", "None", "", "", "", "", function()
+    loadstring(game:HttpGetAsync("https://pastebin.com/raw/tj7dCzAK"))()
+end)
+
+Main_:CreateButton("Neko Maid", "None", "", "", "", "", function()
+    loadstring(game:HttpGetAsync("https://paste.ee/d/iCtCM/0"))()
+end)
+
+Main_:CreateButton("Invis", "None", "", "", "", "", function()
+    loadstring(game:HttpGetAsync("https://pastebin.com/raw/uc6nN23y"))()
+end)

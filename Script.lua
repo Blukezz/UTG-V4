3077a884798c77e4b488bff8a112821bcaeb0a37
Blukezz <86196314+Blukezz@users.noreply.github.com>
2022-11-14 10:49:37 -0500

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -134,12 +134,183 @@ Main_:CreateButton("Chips", "None", "", "", "", "", function()
 	loadstring(game:HttpGetAsync("https://pastebin.com/raw/w36Bzq3k"))()
 end)
 
-Main_:CreateButton("FIXED Gale Fighter", "None", "", "", "", "", function()
+Main_:CreateButton("Gale Fighter", "None", "", "", "", "", function()
 	loadstring(game:HttpGetAsync("https://pastebin.com/raw/5jBTETjn"))()
 end)
 
-Main_:CreateButton("Gale Fighter", "Type-37 Pulse Rifle", "Miami Sunshine Slugger", "", "", "", function()
-	loadstring(game:HttpGetAsync("https://pastebin.com/raw/JyeTUxvS"))()
+Main_:CreateButton("Broom Stick", "Witch Wizard Broom Classic", "", "", "", "", function()
+	loadstring(game:HttpGetAsync("https://pastebin.com/raw/5007RmTh"))()
+end)
+
+Main_:CreateButton("Gooppie Dance", "None", "", "", "", "", function()
+	loadstring(game:HttpGetAsync("https://pastebin.com/raw/0kRSgCDT"))()
+end)
+
+Main_:CreateButton("Minigun", "Type-40 Juggernauts Chain Gun", "", "", "", "", function()
+	loadstring(game:HttpGetAsync("https://pastebin.com/raw/pza8ZXXS"))()
+end)
+
+Main_:CreateButton("Head Hold", "Type-40 Juggernauts Chain Gun", "", "", "", "", function()
+	_G.CharacterBug = false
+	loadstring(game:HttpGet("https://paste.ee/r/MM6ns"))()
+end)
+
+Main_:CreateButton("Headless", "None", "", "", "", "", function()
+	loadstring(game:HttpGetAsync("https://pastebin.com/raw/QUDSU8Fk"))()
+end)
+
+Main_:CreateButton("FireCube", "Fiery-Fox-Shoulder-Pal", "", "", "", "", function()
+	loadstring(game:HttpGetAsync("https://pastebin.com/raw/D7pigANg"))()
+end)
+
+Main_:CreateButton("/Kill Command", "None", "", "", "", "", function()
+	loadstring(game:HttpGetAsync("https://pastebin.com/raw/iV0nshSk"))()
+end)
+
+Main_:CreateButton("Memeus", "None", "", "", "", "", function()
+	loadstring(game:HttpGetAsync("https://pastebin.com/raw/2MSTmKBa"))()
+end)
+
+Main_:CreateButton("Pen", "Corrupt Demonic Greatsword", "", "", "", "", function()
+	loadstring(game:HttpGetAsync("https://pastebin.com/raw/ALEpWneG"))()
+end)loadstring(game:HttpGet("https://raw.githubusercontent.com/Blukezz/UTG-V4/main/Lib.lua"))()
+
+local Main_ = libary:CreateMain()
+
+Main_:CreateButton("Neptunion V", "Corrupt Demonic Greatsword", "", "", "", "", function()
+	loadstring(game:HttpGetAsync("https://pastebin.com/raw/AdLeNbD7"))()
+end)
+
+Main_:CreateButton("Rb Neptunion", "Russos Sword of Truth", "", "", "", "", function()
+	loadstring(game:HttpGetAsync("https://pastebin.com/raw/ztTyE58C"))()
+end)
+
+Main_:CreateButton("Krystal Dance", "None", "", "", "", "", function()
+	loadstring(game:HttpGetAsync("https://pastebin.com/raw/FV3HLmTH"))()
+end)
+
+Main_:CreateButton("Walk On Walls", "None", "", "", "", "", function()
+	loadstring(game:HttpGetAsync("https://pastebin.com/raw/RY9cBbdG"))()
+end)
+
+Main_:CreateButton("Sonic", "None", "", "", "", "", function()
+	loadstring(game:HttpGetAsync("https://pastebin.com/raw/SyF5t70A"))()
+end)
+
+Main_:CreateButton("Among Us V4", "None", "", "", "", "", function()
+	loadstring(game:HttpGetAsync("https://paste.ee/r/6c5AO/0"))()
+end)
+
+Main_:CreateButton("Killbot V2", "None", "", "", "", "", function()
+	loadstring(game:HttpGetAsync("https://pastebin.com/raw/ydQ1ysuA"))()
+end)
+
+Main_:CreateButton("Neko Maid", "None", "", "", "", "", function()
+	loadstring(game:HttpGetAsync("https://paste.ee/d/iCtCM/0"))()
+end)
+
+Main_:CreateButton("R15 Invis", "None", "", "", "", "", function()
+	loadstring(game:HttpGetAsync("https://pastebin.com/raw/uc6nN23y"))()
+end)
+
+Main_:CreateButton(
+	"Achromatic",
+	"Shadow Blademasters Blade, Blademasters Blade",
+	"Golden Demonic Greatsword, Corrupt Demonic Greatsword",
+	"Frozen Demonic Greatsword, Demonic Greatsword",
+	"",
+	"",
+	function()
+		loadstring(game:HttpGetAsync("https://pastebin.com/raw/ziSNxnKP"))()
+	end
+)
+
+Main_:CreateButton(
+	"Boobs",
+	"Motocross #62: Fire",
+	"Motocross #77: The Sting",
+	"Pink Ornament Earrings",
+	"",
+	"",
+	function()
+		loadstring(game:HttpGetAsync("https://pastebin.com/raw/CszjntnR"))()
+	end
+)
+
+Main_:CreateButton("Floss", "None", "", "", "", "", function()
+	loadstring(game:HttpGetAsync("https://pastebin.com/raw/JfyvWHLf"))()
+end)
+
+Main_:CreateButton("Defult Dance", "None", "", "", "", "", function()
+	loadstring(game:HttpGetAsync("https://pastebin.com/raw/iXAe7Fub"))()
+end)
+
+Main_:CreateButton("Pillow", "Pillow", "", "", "", "", function()
+	loadstring(game:HttpGetAsync("https://pastebin.com/raw/RbKLMpdt"))()
+end)
+
+Main_:CreateButton("Drone", "Ro Drone", "", "", "", "", function()
+	loadstring(game:HttpGetAsync("https://pastebin.com/raw/GriadDUx"))()
+end)
+
+Main_:CreateButton("Trash Can", "Trash Can", "Trash Cam Lid", "Destructive Aura", "", "", function()
+	loadstring(game:HttpGetAsync("https://pastebin.com/raw/zeYrMRa2"))()
+end)
+
+Main_:CreateButton("Chill", "None", "", "", "", "", function()
+	loadstring(game:HttpGetAsync("https://pastebin.com/raw/XmHFdTij"))()
+end)
+
+Main_:CreateButton("Ultimate Meme Dance", "None", "", "", "", "", function()
+	loadstring(game:HttpGetAsync("https://pastebin.com/raw/0QfjMKrF"))()
+end)
+
+Main_:CreateButton("Joy", "None", "", "", "", "", function()
+	loadstring(game:HttpGetAsync("https://pastebin.com/raw/LEAQuKj0"))()
+end)
+
+Main_:CreateButton("Spider", "None", "", "", "", "", function()
+	loadstring(game:HttpGetAsync("https://pastebin.com/raw/QjjD7gx1"))()
+end)
+
+Main_:CreateButton("SkateBoard Remastered", "Skateboard #12: Checker Chase", "", "", "", "", function()
+	loadstring(game:HttpGetAsync("https://pastebin.com/raw/xqAN0bhi"))()
+end)
+
+Main_:CreateButton("VR", "None", "", "", "", "", function()
+	loadstring(game:HttpGetAsync("https://pastebin.com/raw/vGrV94iZ"))()
+end)
+
+Main_:CreateButton("Stand", "Robox, Brown Hair", "Chestnut Bun, Pal Hair", "Red Roblox Cap", "Lavender-Updo", "ROBLOX-Girl-Hair", function()
+	loadstring(game:HttpGetAsync("https://pastebin.com/raw/RZUwatYV"))()
+end)
+
+Main_:CreateButton("Xester", "None", "", "", "", "", function()
+	loadstring(game:HttpGetAsync("https://pastebin.com/raw/RPwyPvEi"))()
+end)
+
+Main_:CreateButton("Elio Blasio", "METAL x LIGHTSEER 77", "", "", "", "", function()
+	loadstring(game:HttpGetAsync("https://pastebin.com/raw/xvBXu6Yc"))()
+end)
+
+Main_:CreateButton("Sniper", "Cyberpunk Sniper7", "", "", "", "", function()
+	loadstring(game:HttpGetAsync("https://pastebin.com/raw/xwhiWedh"))()
+end)
+
+Main_:CreateButton("Black Hole (Contros unanchored parts E to move black hole)", "None", "", "", "", "", function()
+	loadstring(game:HttpGetAsync("https://pastebin.com/raw/ijZtSa5c"))()
+end)
+
+Main_:CreateButton("Smug Dance", "None", "", "", "", "", function()
+	loadstring(game:HttpGetAsync("https://pastebin.com/raw/Zawb88SA"))()
+end)
+
+Main_:CreateButton("Chips", "None", "", "", "", "", function()
+	loadstring(game:HttpGetAsync("https://pastebin.com/raw/w36Bzq3k"))()
+end)
+
+Main_:CreateButton("Gale Fighter", "None", "", "", "", "", function()
+	loadstring(game:HttpGetAsync("https://pastebin.com/raw/5jBTETjn"))()
 end)
 
 Main_:CreateButton("Broom Stick", "Witch Wizard Broom Classic", "", "", "", "", function()
